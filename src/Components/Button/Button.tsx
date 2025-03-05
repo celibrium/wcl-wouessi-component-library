f374dc69f0428feacce963bf7b4a2cdc7bec87ee
Muhammad Ali Hafeez <muhammadalihafeez@Muhammads-MacBook-Air.local>
2025-03-04 19:57:56 -0500

    Updated Button component to support prop icon

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -2,17 +2,20 @@ import { ReactNode } from 'react';
 import styles from './Button.module.scss';
 import clsx from 'clsx';
 
-const Button = ({ children, className, variant = 'primary' }: ButtonProps) => {
-  const btnClassName = `btn ${className}`;
+const Button = ({ children, className, variant = 'primary', icon }: ButtonProps) => {
   return (
-    <button className={clsx(styles.btn, btnClassName, variant == 'primary' && styles.primary)}>{ children }</button>
+    <button className={clsx(styles.btn, className, variant === 'primary' && styles.primary)}>
+      {icon && <span className={styles.icon}>{icon}</span>} {/* Render icon as a ReactNode */}
+      {children}
+    </button>
   );
 };
 
 export type ButtonProps = {
   children: ReactNode;
-  className?: string,
-  variant?: 'primary' | 'secondary'
+  className?: string;
+  variant?: 'primary' | 'secondary';
+  icon?: ReactNode; //  Icon is now a ReactNode, so it can accept strings or React elements
 };
 
 export default Button;
