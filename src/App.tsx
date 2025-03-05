4a4057e75e3fd9a75c99084de61b6ce3c2e33ce3
Stanley Cao <stanleycao23@gmail.com>
2025-03-05 01:53:30 -0500

    Created Icon Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,17 @@
 import { BrowserRouter as Router, Route, Routes, NavLink } from "react-router-dom";
 import './App.css'
 import Button from './Components/Button';
-
-
-
+import { Icon } from './Components/Icon';
 
 const Route1 = () => (
   <div>
     <h1>Route 1</h1> 
+    <Icon name="CaretCircleRight" size={96}/> 
     <nav>
     <NavLink to="/">Route 1</NavLink> | {' '}
     <NavLink to="/route2">Route 2</NavLink>
     </nav>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magnam sint molestias. Aliquid, nihil. Quisquam nisi ullam excepturi vel sequi, fugiat iusto, totam dolores architecto expedita, laudantium quam iste dolore.</p>
+    <p>1. Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem magnam sint molestias. Aliquid, nihil. Quisquam nisi ullam excepturi vel sequi, fugiat iusto, totam dolores architecto expedita, laudantium quam iste dolore.</p>
   </div>
 );
 
