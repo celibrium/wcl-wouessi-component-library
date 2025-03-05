e8802e56897be903e04c0b9fdfeb26d7d2a58c4a
vrundacvaghasiya <vrundacvaghasiya@gmail.com>
2025-03-05 11:17:27 -0500

    Add the block boolean parameter

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -2,17 +2,18 @@ import { ReactNode } from 'react';
 import styles from './Button.module.scss';
 import clsx from 'clsx';
 
-const Button = ({ children, className, variant = 'primary' }: ButtonProps) => {
+const Button = ({ children, className, variant = 'primary', block = false }: ButtonProps) => {
   const btnClassName = `btn ${className}`;
   return (
-    <button className={clsx(styles.btn, btnClassName, variant == 'primary' && styles.primary)}>{ children }</button>
+    <button className={block ? clsx(styles.btn, btnClassName, variant == 'primary' && styles.primary) : ""}>{children}</button>
   );
 };
 
 export type ButtonProps = {
   children: ReactNode;
   className?: string,
-  variant?: 'primary' | 'secondary'
+  variant?: 'primary' | 'secondary',
+  block?: boolean
 };
 
 export default Button;
