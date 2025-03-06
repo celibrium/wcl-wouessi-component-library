efdf64f3586cbbae1ee309895468d8d0c6e82623
Eric Njanga <eric.njanga@gmail.com>
2025-03-06 13:21:52 -0500

    Ensure all stories get app fonts and styling

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,10 @@
-import type { Preview } from '@storybook/react'
+import React from 'react';
+import type { Preview } from '@storybook/react';
+import "../src/App.css"; // Ensure global styles are loaded
 
 const preview: Preview = {
   parameters: {
+    layout: "centered",
     controls: {
       matchers: {
        color: /(background|color)$/i,
@@ -9,6 +12,27 @@ const preview: Preview = {
       },
     },
   },
+  decorators: [
+    (Story) => 
+      React.createElement(
+        React.Fragment,
+        {},
+        React.createElement("link", {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        }),
+        React.createElement("link", {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+          crossOrigin: "anonymous",
+        }),
+        React.createElement("link", {
+          href: "https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap",
+          rel: "stylesheet",
+        }),
+        React.createElement(Story)
+      ),
+  ],
 };
 
 export default preview;
