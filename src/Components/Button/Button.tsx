36892439ca821f5e5aa264f5a1fe833c99d0b575
Stanley Cao <stanleycao23@gmail.com>
2025-03-07 01:05:42 -0500

    Turn the button component into icon

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import styles from './Button.module.scss';
 import clsx from 'clsx';
+import SpeakerIcon from '../../assets/Speaker.svg?react'; 
 
 
 /**
@@ -15,35 +16,42 @@ const Button = ({
   icon, 
   className = '', 
   variant = 'primary', 
-  block = false 
+  block = false,
+  as = 'button' 
 }: ButtonProps) => {
+  
+  const isIconButton = as === 'button icon';
 
-  let btnVariantClass = '';
-
-  switch(variant) {
-    case 'primary': btnVariantClass = styles.primary; break;
-    case 'secondary': btnVariantClass = styles.secondary; break;
-    case 'tertiary': btnVariantClass = styles.tertiary; break;
-  }
-
-
-  const blockClass = block ? styles.block : '';            // Assign block class if button is a block
-  const btnClassName = `btn ${blockClass} ${btnVariantClass} ${className}`;  // Combine all classes into 1
+  const btnClassName = clsx(
+    isIconButton ? styles.iconOnly : styles.btn, 
+    className,
+    {
+      [styles.primary]: !isIconButton && variant === 'primary',
+      [styles.secondary]: !isIconButton && variant === 'secondary',
+      [styles.tertiary]: !isIconButton && variant === 'tertiary',
+      [styles.block]: !isIconButton && block
+    }
+  );
 
   return (
-    <button className={clsx(styles.btn, btnClassName, variant === 'primary' && styles.primary)}>
-      {icon && <span className={styles.icon}>{icon}</span>} {/* Render icon as a ReactNode */}
-      {children}
+    <button className={btnClassName}>
+      {isIconButton ? <SpeakerIcon className={styles.largeIcon} /> : (
+        <>
+          {icon && <span className={styles.icon}>{icon}</span>}
+          {children}
+        </>
+      )}
     </button>
   );
 };
 
 export type ButtonProps = {
-  children: ReactNode;
-  className?: string,
-  variant?: 'primary' | 'secondary' | 'tertiary',
-  block?: boolean
-  icon?: ReactNode; //  Icon is now a ReactNode, so it can accept strings or React elements
+  children?: ReactNode;
+  className?: string;
+  variant?: 'primary' | 'secondary' | 'tertiary';
+  block?: boolean;
+  icon?: ReactNode;
+  as?: 'button' | 'button icon';
 };
 
 export default Button;
